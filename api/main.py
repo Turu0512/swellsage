cafc49558b9ef7de3e537673c5ca2ef3accfb7cc
Turu0512 <hinataptyan@gmail.com>
2024-10-01 07:39:00 +0900

    ルーターを追加

--- api/main.py
+++ api/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+from fastapi import FastAPI
+from routers import chat
+
+app = FastAPI()
+
+# ルーターを追加
+app.include_router(chat.router)
